e020b8cb61821113e8e25899d0ab929d36245311
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2024-01-15 22:30:49 +0600

    done

---
+++ resources/js/Pages/Test.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <button @click="count++">+</button>
+        {{ count }}
+        <button @click="count--">-</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Test",
+    data() {
+        return {
+            count: 0,
+        };
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
